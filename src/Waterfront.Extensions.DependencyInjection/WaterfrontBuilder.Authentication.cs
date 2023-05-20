87f358c8c0d0999d53b0f041409aeba5803b6e2a
2CHEVSKII <prsroman3@gmail.com>
2023-05-20 22:44:41 +0300

    Found out about ActivatorUtilities - happy now

---
+++ src/Waterfront.Extensions.DependencyInjection/WaterfrontBuilder.Authentication.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Waterfront.Common.Authentication;
+using Waterfront.Common.Authorization;
+using Waterfront.Core.Authentication;
+using Waterfront.Core.Authorization;
+
+namespace Waterfront.Extensions.DependencyInjection;
+
+public partial class WaterfrontBuilder
+{
+    public IWaterfrontBuilder WithAuthentication<TService>() where TService : class, IAclAuthenticationService
+    {
+        Services.AddScoped<IAclAuthenticationService, TService>();
+        return this;
+    }
+
+    public IWaterfrontBuilder WithAuthentication<TService, TOptions>(string name, Action<TOptions> configureOptions)
+        where TService : AclAuthenticationServiceBase<TOptions> where TOptions : class
+    {
+        Services.AddScoped<IAclAuthenticationService, TService>(
+            sp =>
+            {
+                TOptions options = sp.GetRequiredService<IOptionsSnapshot<TOptions>>().Get(name);
+                TService service = ActivatorUtilities.CreateInstance<TService>(sp, options);
+
+                return service;
+            }
+        );
+        Services.AddSingleton<IConfigureOptions<TOptions>>(new ConfigureNamedOptions<TOptions>(name, configureOptions));
+        return this;
+    }
+
+    public IWaterfrontBuilder WithAuthentication<TService, TOptions>(Action<TOptions> configureOptions)
+        where TService : AclAuthenticationServiceBase<TOptions> where TOptions : class =>
+        WithAuthentication<TService, TOptions>(Options.DefaultName, configureOptions);
+}
+
---
+++ src/Waterfront.Extensions.DependencyInjection/WaterfrontBuilder.Authorization.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Waterfront.Common.Authorization;
+using Waterfront.Core.Authorization;
+
+namespace Waterfront.Extensions.DependencyInjection;
+
+public partial class WaterfrontBuilder
+{
+    public IWaterfrontBuilder WithAuthorization<TService>() where TService : class, IAclAuthorizationService
+    {
+        Services.AddScoped<IAclAuthorizationService, TService>();
+        return this;
+    }
+
+    public IWaterfrontBuilder WithAuthorization<TService, TOptions>(string name, Action<TOptions> configureOptions)
+        where TService : AclAuthorizationServiceBase<TOptions> where TOptions : class
+    {
+        Services.AddScoped<IAclAuthorizationService, TService>(
+            sp =>
+            {
+                TOptions options = sp.GetRequiredService<IOptionsSnapshot<TOptions>>().Get(name);
+                TService service = ActivatorUtilities.CreateInstance<TService>(sp, options);
+
+                return service;
+            }
+        );
+        Services.AddSingleton<IConfigureOptions<TOptions>>(new ConfigureNamedOptions<TOptions>(name, configureOptions));
+        return this;
+    }
+
+    public IWaterfrontBuilder WithAuthorization<TService, TOptions>(Action<TOptions> configureOptions)
+        where TService : AclAuthorizationServiceBase<TOptions> where TOptions : class =>
+        WithAuthorization<TService, TOptions>(Options.DefaultName, configureOptions);
+}
