a325bdd4475a9ae4ef0a6b68ad2ab5aa0696ef55
2CHEVSKII <prsroman3@gmail.com>
2023-07-05 23:54:55 +0300

    Implement basic version of AclAuthenticationHandlerProvider

---
+++ src/Waterfront.Extensions.DependencyInjection/Authentication/AclAuthenticationHandlerProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Waterfront.Common.Authentication;
+
+namespace Waterfront.Extensions.DependencyInjection.Authentication;
+
+// TODO: Implement caching
+
+public class AclAuthenticationHandlerProvider : IAclAuthenticationHandlerProvider
+{
+    protected ILogger Logger { get; }
+    protected IServiceProvider ServiceProvider { get; }
+
+    public AclAuthenticationHandlerProvider(ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
+    {
+        Logger = loggerFactory.CreateLogger(GetType());
+        ServiceProvider = serviceProvider;
+    }
+
+    public Task<IAclAuthenticationHandler> GetHandlerAsync(AclAuthenticationScheme scheme)
+    {
+        object handler = ActivatorUtilities.CreateInstance(ServiceProvider, scheme.HandlerType);
+        return Task.FromResult((IAclAuthenticationHandler)handler);
+    }
+}
