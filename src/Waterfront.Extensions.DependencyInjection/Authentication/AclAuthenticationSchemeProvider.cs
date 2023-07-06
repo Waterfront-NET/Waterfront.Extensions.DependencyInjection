0706aa2a48dc509f04d7ee650373851f81a8934a
2CHEVSKII <prsroman3@gmail.com>
2023-07-06 08:10:44 +0300

    Add AclAuthenticationSchemeProvider

---
+++ src/Waterfront.Extensions.DependencyInjection/Authentication/AclAuthenticationSchemeProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Waterfront.Common.Authentication;
+using Waterfront.Core.Authentication;
+
+namespace Waterfront.Extensions.DependencyInjection.Authentication;
+
+public class AclAuthenticationSchemeProvider : AclAuthenticationSchemeProviderBase
+{
+    private readonly IOptions<AclAuthenticationOptions> _options;
+
+    public override IReadOnlyCollection<AclAuthenticationScheme> Schemes => _options.Value.Schemes;
+    public override IReadOnlyDictionary<string, AclAuthenticationScheme> SchemeMap => _options.Value.SchemeMap;
+    protected ILogger Logger { get; }
+
+    public AclAuthenticationSchemeProvider(ILoggerFactory loggerFactory, IOptions<AclAuthenticationOptions> options)
+    {
+        Logger = loggerFactory.CreateLogger(GetType());
+        _options = options;
+    }
+}
